77122322167ff3354c39a125e82ea147695921bd
Ashley Sharp <ashley.sharpuk@gmail.com>
2016-11-12 15:02:57 +0000

    first commit

---
+++ PM2.5.R
@@ -0,0 +1,6 @@
+setwd("/Users/ashleysharp/Dropbox/R/Coursera/exploratory-analysis/PM2.5")
+
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
